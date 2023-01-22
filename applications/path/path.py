4dbac0a2973464414635de28f3fc1a602c779602
daniel <tishakovdanyilgmail.com>
2023-01-22 19:38:39 +0000

    docker run

---
+++ applications/path/path.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from typing import Final
+
+ROOT_PATH:Final[Path] = Path(__file__).parents[2]
+FILES_INPUT_FILES: Final[Path] = ROOT_PATH.joinpath("files_input")
+FILES_OUTPUT_FILES: Final[Path] = ROOT_PATH.joinpath("output_input")
+
+print(FILES_INPUT_FILES)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import requests
+import json
+
 
 def get_info():
     response = requests.get("http://api.open-notify.org/astros.json")
     with open("info", "w") as file:
         file.write(response.text)
-    return response.text
+    answer = json.loads(response.text)
+    for i in answer["people"]:
+        print(i["name"])
 
 def main():
     print(get_info())
